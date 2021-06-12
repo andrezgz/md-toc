e81f66e2d89045510556bc8d8c8db32f363c5c64
andrezgz <andrezgz@gmail.com>
2021-06-12 00:11:50 -0300

    Added titleToFN

---
+++ titleToFilename.js
@@ -0,0 +1,37 @@
+function fnIt(inputMD)
+{
+    inputMDLines = inputMD.split("\n");
+    var outputMD = "";
+
+    for(var i = 0; i < inputMDLines.length; ++i)
+    {
+        var inputMDLine = inputMDLines[i];
+
+        var match = /^(#+) (.+)$/.exec(inputMDLine);
+        if(match)
+        {
+            var headingLevel = match[1].length;
+            var headingTitle = match[2].replace(/<.*?>/g, "");
+
+            if(headingLevel != 1)
+            {
+                continue;
+            }
+
+            outputMD = headingTitle
+                        .replace(/[.\/]/g, "-")
+                        .replace(/[áéíóúüñÁÉÍÓÚÜÑ]/g, function (m) {
+                            return {
+                                "á": "a", "é": "e", "í": "i", "ó": "o", "ú": "u", "ü": "u", "ñ": "n",
+                                "Á": "A", "É": "E", "Í": "I", "Ó": "O", "Ú": "U", "Ü": "U", "Ñ": "N"
+                            }[m];
+                        })
+                        .replace(/[^a-zA-Z0-9\-]/g, "_")
+                        .replace(/[_-]+/g, "-")
+                        .replace(/\-$/, "");
+
+        }
+    }
+
+    return outputMD;
+}
